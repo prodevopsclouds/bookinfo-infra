d972198db22815e3c4d6c22a33c26b5603d4f20c
Amir Khan <162446922+prodevopsclouds@users.noreply.github.com>
2026-02-13 16:40:09 +0530

    Add outputs for cluster info and application URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,25 +1,7 @@
-output "kubernetes_cluster_name" {
-  value       = google_container_cluster.primary.name
-  description = "GKE Cluster Name"
+output "cluster_info" {
+  value = var.cluster_info
 }
 
-output "kubernetes_cluster_host" {
-  value       = google_container_cluster.primary.endpoint
-  description = "GKE Cluster Host"
-  sensitive   = true
-}
-
-output "region" {
-  value       = var.gcp_region
-  description = "GCP region"
-}
-
-output "project_id" {
-  value       = var.gcp_project_id
-  description = "GCP Project ID"
-}
-
-output "kubectl_config_command" {
-  value       = "gcloud container clusters get-credentials ${google_container_cluster.primary.name} --zone ${var.cluster_zone} --project ${var.gcp_project_id}"
-  description = "Command to configure kubectl"
+output "application_url" {
+  value = var.application_url
 }
